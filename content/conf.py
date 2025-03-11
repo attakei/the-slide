3fed238e30234ef4587e872f619a60bf06038e55
Kazuya Takei <myself@attakei.net>
2025-03-11 22:02:25 +0900

    feat: Configure for screenshot as og:image

--- content/conf.py
+++ content/conf.py
@@ -1,4 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+
 # -- Project information
 project = "the-slide"
 copyright = "2024, Kazuya Takei"
@@ -13,6 +15,7 @@ extensions = [
     # My extensions
     "oembedpy.adapters.sphinx",
     "sphinx_revealjs",
+    "sphinx_revealjs.ext.screenshot",
     # Third-party extensions
     "pyvista.ext.plot_directive",
     "pyvista.ext.viewer_directive",
@@ -22,6 +25,7 @@ extensions = [
     "sphinxcontrib.asciinema",
     "sphinxcontrib.mermaid",
     "sphinxemoji.sphinxemoji",
+    "sphinxext.opengraph",
     # Itself
     "the_slide",
 ]
@@ -58,3 +62,11 @@ pygments_style = "monokai"
 mermaid_version = "11.4.1"
 mermaid_output_format = "svg"
 mermaid_cmd = "pnpm mmdc -p puppeteer-config.json"
+# sphinxext.opengraph
+ogp_site_url = os.environ.get("SITE_URLBASE", "http://localhost:8000/")
+ogp_type = "article"
+ogp_custom_meta_tags = [
+    '<meta name="twitter:card" content="summary_large_image" >',
+    '<meta name="twitter:site" content="@attakei" >',
+]
+ogp_enable_meta_description = True
