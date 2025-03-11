81a800307fa7e6fe517614214812c42e9b59a198
Kazuya Takei <myself@attakei.net>
2025-03-11 21:58:55 +0900

    refactor: Remove func to disable as extension

--- content/conf.py
+++ content/conf.py
@@ -58,7 +58,3 @@ pygments_style = "monokai"
 mermaid_version = "11.4.1"
 mermaid_output_format = "svg"
 mermaid_cmd = "pnpm mmdc -p puppeteer-config.json"
-
-
-def setup(app) -> dict:
-    print("Working this extension!")
